0172b9be9c598d07414b89c26456fde6f4639766
Ryohei Ikegami <iofg2100@gmail.com>
2017-04-15 14:04:31 +0900

    ShapeSelect

---
+++ src/core/views/ShapeSelect.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+const styles = require('./ShapeSelect.css')
+
+export
+class ShapeSelect extends React.Component<{}, {}> {
+  render () {
+    return (
+      <div className={styles.ShapeSelect}>
+      </div>
+    )
+  }
+}
