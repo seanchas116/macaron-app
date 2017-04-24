e800f63a0671ba34f69711e0bbc0e8b67656ccdc
minamorl <minamorl@users.noreply.github.com>
2017-04-25 07:49:43 +0900

    Use foreignObject instead of text

--- src/core/items/TextItem.tsx
+++ src/core/items/TextItem.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import {Vec2} from 'paintvec'
-import {computed, observable} from 'mobx'
+import {observable} from 'mobx'
 import {Item} from './Item'
 import {Movable} from '../drawarea/Movable'
 
@@ -9,23 +9,15 @@ class TextItem extends Item {
   @observable position = new Vec2()
   @observable size = new Vec2()
   name = 'Text'
-
-  @computed get scale() {
-    if(this.originalSize) {
-      const sx = this.size.width / this.originalSize.width
-      const sy = this.size.height / this.originalSize.height
-      return `scale(${sx}, ${sy})`
-    }
-    return ''
-  }
+  @observable text = 'Text'
 
   render (): JSX.Element {
     const {x, y} = this.position
     return <Movable item={this} key={this.id}>
-      <text
-        x={x} y={y} transform={this.scale}>
-        test
-      </text>
+      <foreignObject
+        x={x} y={y} width={this.size.width} height={this.size.height}>
+        <div style={{width:'100%'}} contentEditable>{this.text}</div>
+      </foreignObject>
     </Movable>
   }
 
