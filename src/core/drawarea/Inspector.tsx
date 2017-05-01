4aefd07640fe24629e4bb8bd5aee2ea3d83d1545
Ryohei Ikegami <iofg2100@gmail.com>
2017-05-01 13:41:12 +0900

    Add Inspector

---
+++ src/core/drawarea/Inspector.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+const styles = require('./Inspector.css')
+
+export class Inspector extends React.Component<{}, {}> {
+
+  render () {
+    return <div className={styles.root}>
+    </div>
+  }
+}
