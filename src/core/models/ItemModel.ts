f15fca60f4386cfdcc36d27ce70e00f1b37d705a
Ryohei Ikegami <iofg2100@gmail.com>
2017-05-04 18:01:37 +0900

    Store as json

--- src/core/models/ItemModel.ts
+++ src/core/models/ItemModel.ts
@@ -1,13 +1,7 @@
-import {Entity, TableInheritance, DiscriminatorColumn, PrimaryColumn, Column} from 'typeorm'
+import {Entity, PrimaryColumn, Column} from 'typeorm'
 
 @Entity('item')
-@TableInheritance('single-table')
-@DiscriminatorColumn({name: 'type', type: 'string'})
 export abstract class ItemModel {
   @PrimaryColumn('string') id: string
-  @Column() name: string
-  @Column() fill: string
-  @Column() stroke: string
-  @Column() strokeWidth: number
-  @Column() parentId: string
+  @Column('json') data: any
 }
