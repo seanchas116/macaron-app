e0ad56c5826e37c986c030e3111e0f6ef0102b80
Ryohei Ikegami <iofg2100@gmail.com>
2017-05-04 18:52:48 +0900

    ItemData in ItemModel

--- src/core/models/ItemModel.ts
+++ src/core/models/ItemModel.ts
@@ -1,7 +1,8 @@
 import {Entity, PrimaryColumn, Column} from 'typeorm'
+import {ItemData} from '../items/Item'
 
 @Entity('item')
 export abstract class ItemModel {
   @PrimaryColumn('string') id: string
-  @Column('json') data: any
+  @Column('json') data: ItemData
 }
