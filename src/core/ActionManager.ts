790d2e63af55351a52546dad0243e6a60531b145
Ryohei Ikegami <iofg2100@gmail.com>
2017-05-04 20:23:13 +0900

    Add ActionManager

---
+++ src/core/ActionManager.ts
@@ -0,0 +1,18 @@
+import {Action} from './Action'
+
+export default
+class ActionManager {
+  actions = new Map<string, Action>()
+
+  add (...actions: Action[]) {
+    for (const action of actions) {
+      this.actions.set(action.id, action)
+    }
+  }
+}
+
+export const actionManager = new ActionManager()
+
+export function addAction (klass: {new(): Action}) {
+  actionManager.add(new klass())
+}
