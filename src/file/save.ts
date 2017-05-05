cd74db771ed5672034c13cc4f236e4c43a99fb38
Ryohei Ikegami <iofg2100@gmail.com>
2017-05-05 15:52:54 +0900

    Update save

--- src/file/save.ts
+++ src/file/save.ts
@@ -2,6 +2,8 @@ import * as fs from 'fs'
 import * as temp from 'temp'
 import * as JSZip from 'jszip'
 import {Document} from '../core/Document'
+import {Item, ItemProps} from '../core/items/Item'
+import {GroupItem} from '../core/items/GroupItem'
 import * as msgpack from 'msgpack-lite'
 
 interface DocumentData {
@@ -10,6 +12,21 @@ interface DocumentData {
   scrollX: number
   scrollY: number
   pages: {name: string, path: string}[]
+}
+
+interface ItemData extends ItemProps {
+  id: string
+  children?: ItemData[]
+}
+
+function itemToData (item: Item): ItemData {
+  const props = item.toProps()
+  const {id} = item
+  if (item instanceof GroupItem) {
+    return {...props, id, children: item.children.map(itemToData)}
+  } else {
+    return {...props, id}
+  }
 }
 
 export async function save (document: Document, filePath: string) {
@@ -31,7 +48,7 @@ export async function save (document: Document, filePath: string) {
   }
   zip.file('document.msgpack', msgpack.encode(documentData))
 
-  const pageData = msgpack.encode(document.rootItem.toData())
+  const pageData = msgpack.encode(itemToData(document.rootItem))
   zip.file(pagePath, pageData)
 
   const tempPath = temp.path()
