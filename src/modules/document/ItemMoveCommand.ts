0162e6434beb4c9bf1b17bbec3ea1d72b518de23
Ryohei Ikegami <iofg2100@gmail.com>
2017-05-07 12:31:29 +0900

    Fix ItemMoveCommand

--- src/modules/document/ItemMoveCommand.ts
+++ src/modules/document/ItemMoveCommand.ts
@@ -4,6 +4,7 @@ import {GroupItem} from '../document/GroupItem'
 
 export class ItemMoveCommand extends Command {
   title = 'Move Item'
+  originalParent: GroupItem
   originalBeforeReference: Item|undefined
 
   constructor (public parent: GroupItem, public item: Item, public beforeReference: Item|undefined) {
@@ -15,11 +16,14 @@ export class ItemMoveCommand extends Command {
     if (!origParent) {
       throw new Error('item is root and cannot be moved')
     }
+    this.originalParent = origParent
     this.originalBeforeReference = origParent.childAt(origParent.children.indexOf(this.item) + 1)
+    origParent.removeChild(this.item)
     this.parent.insertBefore(this.item, this.beforeReference)
   }
 
   undo () {
-    this.parent.insertBefore(this.item, this.originalBeforeReference)
+    this.parent.removeChild(this.item)
+    this.originalParent.insertBefore(this.item, this.originalBeforeReference)
   }
 }
