42276e52d0ee475cad1a5269f13ca1381c270262
Ryohei Ikegami <iofg2100@gmail.com>
2017-05-08 17:12:18 +0900

    Add RectItem#radius

--- src/modules/document/items/RectItem.ts
+++ src/modules/document/items/RectItem.ts
@@ -1,21 +1,26 @@
+import {observable} from 'mobx'
 import {RectLikeItem, RectLikeItemProps} from './RectLikeItem'
 
 export interface RectItemProps extends RectLikeItemProps {
   type: 'rect'
+  radius: number
 }
 
 export
 class RectItem extends RectLikeItem {
   name = 'Rectangle'
+  @observable radius = 0
 
   clone () {
     return new RectItem(this.document, this.toProps())
   }
 
   toProps (): RectItemProps {
+    const {radius} = this
     return {
       ...super.toProps(),
-      type: 'rect'
+      type: 'rect',
+      radius
     }
   }
 }
