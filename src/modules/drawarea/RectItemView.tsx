4776267f21025bd696c86d939be0d4d3892b35d2
Ryohei Ikegami <iofg2100@gmail.com>
2017-05-08 17:13:36 +0900

    Apply radius in RectItemView

--- src/modules/drawarea/RectItemView.tsx
+++ src/modules/drawarea/RectItemView.tsx
@@ -13,12 +13,14 @@ export class RectItemView extends React.Component<{item: RectItem}, {}> {
     const {width, height} = preview.size
     const fill = preview.fillEnabled ? preview.fill : 'none'
     const stroke = preview.strokeEnabled ? preview.stroke : 'none'
+    const {radius} = preview
     return <Movable item={item} key={item.id}>
       <rect
         x={x} y={y} width={width} height={height}
         fill={fill}
         stroke={stroke}
         strokeWidth={preview.strokeWidth}
+        rx={radius} ry={radius}
       />
     </Movable>
   }
