725f7e4c8a679821bba900fdde0fed925606344e
Ryohei Ikegami <iofg2100@gmail.com>
2017-05-08 17:22:31 +0900

    Refactor inspector

--- src/modules/ui/Inspector.tsx
+++ src/modules/ui/Inspector.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import {observer} from 'mobx-react'
 import {Rect} from 'paintvec'
-import {documentManager, ItemChangeCommand} from '../document'
+import {documentManager, ItemChangeCommand, Item} from '../document'
 const styles = require('./Inspector.css')
 
 interface ValueInputProps {
@@ -45,19 +45,78 @@ class ValueInput extends React.Component<ValueInputProps, {}> {
   }
 }
 
-function ColorInput (props: {value: string, onChange: (value: string) => void}) {
+const ColorInput = (props: {value: string, onChange: (value: string) => void}) => {
   const onChange = (e: React.FormEvent<HTMLInputElement>) => {
     props.onChange(e.currentTarget.value)
   }
   return <input type='color' value={props.value} onChange={onChange} />
 }
 
-function CheckboxInput (props: {value: boolean, onChange: (value: boolean) => void}) {
+const CheckboxInput = (props: {value: boolean, onChange: (value: boolean) => void}) => {
   const onChange = (e: React.FormEvent<HTMLInputElement>) => {
     props.onChange(e.currentTarget.checked)
   }
   return <input type='checkbox' checked={props.value} onChange={onChange} />
 }
+
+
+const RectEdit = observer((props: {item: Item}) => {
+  const {item} = props
+  const {left, top, width, height} = item.rect
+  const {history} = documentManager.document
+
+  const onChangeRect = (title: string, rect: Rect) => {
+    history.push(new ItemChangeCommand(title, item, {rect}))
+  }
+
+  return <div className={styles.rect} >
+    <label>X</label>
+    <ValueInput value={left} onChange={left => onChangeRect('Change X', Rect.fromWidthHeight(left, top, width, height))} />
+    <label>Y</label>
+    <ValueInput value={top} onChange={top => onChangeRect('Change Y', Rect.fromWidthHeight(left, top, width, height))} />
+    <label>W</label>
+    <ValueInput value={width} onChange={width => onChangeRect('Change Width', Rect.fromWidthHeight(left, top, width, height))} />
+    <label>H</label>
+    <ValueInput value={height} onChange={height => onChangeRect('Change Height', Rect.fromWidthHeight(left, top, width, height))} />
+  </div>
+})
+
+const FillEdit = observer((props: {item: Item}) => {
+  const {item} = props
+  const {history} = documentManager.document
+  const onChangeFill = (fill: string) => {
+    history.push(new ItemChangeCommand('Change Fill', item, {fill}))
+  }
+  const onChangeFillEnabled = (fillEnabled: boolean) => {
+    const title = fillEnabled ? 'Enable Fill' : 'Disable Fill'
+    history.push(new ItemChangeCommand(title, item, {fillEnabled}))
+  }
+  return <div className={styles.fill}>
+    <label><CheckboxInput value={item.fillEnabled} onChange={onChangeFillEnabled} />Fill</label>
+    <ColorInput value={item.fill} onChange={onChangeFill} />
+  </div>
+})
+
+const StrokeEdit = observer((props: {item: Item}) => {
+  const {item} = props
+  const {history} = documentManager.document
+  const onChangeStroke = (stroke: string) => {
+    history.push(new ItemChangeCommand('Change Stroke', item, {stroke}))
+  }
+  const onChangeStrokeWidth = (strokeWidth: number) => {
+    history.push(new ItemChangeCommand('Change Stroke Width', item, {strokeWidth}))
+  }
+  const onChangeStrokeEnabled = (strokeEnabled: boolean) => {
+    const title = strokeEnabled ? 'Enable Border' : 'Disable Border'
+    history.push(new ItemChangeCommand(title, item, {strokeEnabled}))
+  }
+  return <div className={styles.stroke}>
+    <label><CheckboxInput value={item.strokeEnabled} onChange={onChangeStrokeEnabled} />Border</label>
+    <ColorInput value={item.stroke} onChange={onChangeStroke} />
+    <ValueInput value={item.strokeWidth} onChange={onChangeStrokeWidth} />
+  </div>
+})
+
 
 @observer
 export class Inspector extends React.Component<{}, {}> {
@@ -68,48 +127,10 @@ export class Inspector extends React.Component<{}, {}> {
       return <div className={styles.root} />
     }
 
-    const onChangeRect = (title: string, rect: Rect) => {
-      document.history.push(new ItemChangeCommand(title, item, {rect}))
-    }
-    const onChangeFill = (fill: string) => {
-      document.history.push(new ItemChangeCommand('Change Fill', item, {fill}))
-    }
-    const onChangeStroke = (stroke: string) => {
-      document.history.push(new ItemChangeCommand('Change Stroke', item, {stroke}))
-    }
-    const onChangeStrokeWidth = (strokeWidth: number) => {
-      document.history.push(new ItemChangeCommand('Change Stroke Width', item, {strokeWidth}))
-    }
-    const onChangeFillEnabled = (fillEnabled: boolean) => {
-      const title = fillEnabled ? 'Enable Fill' : 'Disable Fill'
-      document.history.push(new ItemChangeCommand(title, item, {fillEnabled}))
-    }
-    const onChangeStrokeEnabled = (strokeEnabled: boolean) => {
-      const title = strokeEnabled ? 'Enable Border' : 'Disable Border'
-      document.history.push(new ItemChangeCommand(title, item, {strokeEnabled}))
-    }
-
-    const {left, top, width, height} = item.rect
     return <div className={styles.root}>
-      <div className={styles.rect} >
-        <label>X</label>
-        <ValueInput value={left} onChange={left => onChangeRect('Change X', Rect.fromWidthHeight(left, top, width, height))} />
-        <label>Y</label>
-        <ValueInput value={top} onChange={top => onChangeRect('Change Y', Rect.fromWidthHeight(left, top, width, height))} />
-        <label>W</label>
-        <ValueInput value={width} onChange={width => onChangeRect('Change Width', Rect.fromWidthHeight(left, top, width, height))} />
-        <label>H</label>
-        <ValueInput value={height} onChange={height => onChangeRect('Change Height', Rect.fromWidthHeight(left, top, width, height))} />
-      </div>
-      <div className={styles.fill}>
-        <label><CheckboxInput value={item.fillEnabled} onChange={onChangeFillEnabled} />Fill</label>
-        <ColorInput value={item.fill} onChange={onChangeFill} />
-      </div>
-      <div className={styles.stroke}>
-        <label><CheckboxInput value={item.strokeEnabled} onChange={onChangeStrokeEnabled} />Border</label>
-        <ColorInput value={item.stroke} onChange={onChangeStroke} />
-        <ValueInput value={item.strokeWidth} onChange={onChangeStrokeWidth} />
-      </div>
+      <RectEdit item={item} />
+      <FillEdit item={item} />
+      <StrokeEdit item={item} />
     </div>
   }
 }
