5ef749936431dddd1cbaa68b481b0d24f7a44529
Ryohei Ikegami <iofg2100@gmail.com>
2017-05-09 21:42:17 +0900

    Add typing

--- src/modules/native/index.ts
+++ src/modules/native/index.ts
@@ -1,3 +1,15 @@
 const nbind = require('nbind')
 const {app} = require('electron').remote
-export const {Clipboard} = nbind.init(app.getAppPath()).lib
+const {lib} = nbind.init(app.getAppPath())
+
+export interface Clipboard {
+  clear (): void
+  setData (type: string, data: Buffer): void
+  getData (type: string): Buffer|undefined
+}
+
+interface ClipboardCtor {
+  new (): Clipboard
+}
+
+export const Clipboard: ClipboardCtor = lib.Clipboard
