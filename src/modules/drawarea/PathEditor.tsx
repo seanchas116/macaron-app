a739b2c02a19ab3a7f74bdadfe626ad39c3e4ae9
Ryohei Ikegami <iofg2100@gmail.com>
2017-06-08 16:05:21 +0900

    Refactor

--- src/modules/drawarea/PathEditor.tsx
+++ src/modules/drawarea/PathEditor.tsx
@@ -98,20 +98,18 @@ class PathNodeHandle extends React.Component<{item: PathItem, index: number}, {}
     const h1 = preview.transformPos(node.handle1)
     const h2 = preview.transformPos(node.handle2)
     const selected = item.document.selectedPathNodes.has(index)
-    console.log(index, item.document.selectedPathNodes)
+
+    const positionHandle = <PointerEvents onPointerDown={this.onPointerDownPosition} onPointerMove={this.onPointerMovePosition} onPointerUp={this.onPointerUp} >
+      <circle cx={p.x} cy={p.y} r={4} fill={selected ? 'blue' : 'white'} stroke='grey' />
+    </PointerEvents>
+
     if (node.type === 'straight') {
-      return <g>
-        <PointerEvents onPointerDown={this.onPointerDownPosition} onPointerMove={this.onPointerMovePosition} onPointerUp={this.onPointerUp} >
-          <circle cx={p.x} cy={p.y} r={4} fill={selected ? 'blue' : 'white'} stroke='grey' />
-        </PointerEvents>
-      </g>
+      return <g>{positionHandle}</g>
     } else {
       return <g>
         <line x1={p.x} y1={p.y} x2={h1.x} y2={h1.y} stroke='lightgray' />
         <line x1={p.x} y1={p.y} x2={h2.x} y2={h2.y} stroke='lightgray' />
-        <PointerEvents onPointerDown={this.onPointerDownPosition} onPointerMove={this.onPointerMovePosition} onPointerUp={this.onPointerUp} >
-          <circle cx={p.x} cy={p.y} r={4} fill={selected ? 'blue' : 'white'} stroke='grey' />
-        </PointerEvents>
+        {positionHandle}
         <PointerEvents onPointerDown={this.onPointerDownHandle1} onPointerMove={this.onPointerMoveHandle1} onPointerUp={this.onPointerUp} >
           <circle cx={h1.x} cy={h1.y} r={3} fill='white' stroke='grey' />
         </PointerEvents>
