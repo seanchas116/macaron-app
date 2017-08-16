0caf3bb9cad04754837066ed21f8342695980f2f
Ryohei Ikegami <iofg2100@gmail.com>
2017-08-16 16:39:10 +0900

    Find changes in commit()

--- src/modules/document/VersionControl.ts
+++ src/modules/document/VersionControl.ts
@@ -13,14 +13,31 @@ export class VersionControl {
   constructor (public document: Document) {
   }
 
-  onItemAdd (item: Item) {
-    this.itemSnapshots.set(item.id, item.toData())
-  }
-
-  onItemRemove (item: Item) {
-    this.itemSnapshots.delete(item.id)
-  }
-
   commit (title: string) {
+    const additions: ItemData[] = []
+    for (const id of this.document.itemForId.keys()) {
+      if (!this.itemSnapshots.has(id)) {
+        const data = this.document.itemForId.get(id)!.toData()
+        additions.push(data)
+      }
+    }
+
+    const removals: ItemData[] = []
+    for (const id of this.itemSnapshots.keys()) {
+      if (!this.document.itemForId.has(id)) {
+        removals.push(this.itemSnapshots.get(id)!)
+      }
+    }
+
+    const changes: [ItemData, ItemData][] = []
+    for (const item of this.document.itemForId.values()) {
+      if (item.isDirty) {
+        const oldData = this.itemSnapshots.get(item.id)
+        if (oldData) {
+          const newData = item.toData()
+          changes.push([oldData, newData])
+        }
+      }
+    }
   }
 }
