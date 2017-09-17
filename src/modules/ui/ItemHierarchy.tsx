83a3192fb5ae1456757517320e77532cc04bd5f2
Ryohei Ikegami <iofg2100@gmail.com>
2017-09-17 21:11:59 +0900

    Upgrade react-draggable-tree

--- src/modules/ui/ItemHierarchy.tsx
+++ src/modules/ui/ItemHierarchy.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { TreeView, TreeDelegate, TreeRowInfo } from 'react-draggable-tree'
+import { TreeView, TreeNode, TreeRowInfo } from 'react-draggable-tree'
 import { observer } from 'mobx-react'
 import * as classNames from 'classnames'
 
@@ -7,6 +7,14 @@ import { Item, documentManager, cloneItems } from '../document'
 
 const styles = require('./ItemHierarchy.css')
 require('react-draggable-tree/lib/index.css')
+
+const itemToTreeNode = (item: Item): TreeNode => {
+  return {
+    key: item.id,
+    children: item.canHaveChildren ? item.children.map(itemToTreeNode) : undefined,
+    collapsed: item.collapsed
+  }
+}
 
 @observer
 export class ItemRow extends React.Component<{item: Item, selected: boolean}, {}> {
@@ -19,74 +27,65 @@ export class ItemRow extends React.Component<{item: Item, selected: boolean}, {}
   }
 }
 
-class ItemTreeDelegate implements TreeDelegate<Item> {
-  getKey (item: Item) {
-    return item.id
+@observer
+export class ItemHierarchy extends React.Component<{}, {}> {
+  render () {
+    const document = documentManager.document
+    const selectedKeys = new Set([...document.selectedItems].map(item => item.id))
+
+    return <div className={styles.root}>
+      <TreeView
+        root={itemToTreeNode(document.rootItem)}
+        selectedKeys={selectedKeys}
+        rowHeight={28}
+        renderRow={this.renderRow}
+        onSelectedKeysChange={this.onSelectedKeysChange}
+        onCollapsedChange={this.onCollapsedChange}
+        onContextMenu={this.onContextMenu}
+        onMove={this.onMove}
+        onCopy={this.onCopy}
+      />
+    </div>
   }
-  getChildren (item: Item) {
-    return item.canHaveChildren ? item.children as Item[] : undefined
+
+  private getItem (node: TreeNode) {
+    return documentManager.document.itemForId.get(node.key as string)!
   }
-  getCollapsed (item: Item) {
-    return item.collapsed
+
+  private renderRow = (info: TreeRowInfo) => {
+    return <ItemRow item={this.getItem(info.node)} selected={info.selected} />
   }
-  renderRow (info: TreeRowInfo<Item>) {
-    return <ItemRow item={info.item} selected={info.selected} />
+
+  private onSelectedKeysChange = (selectedKeys: Set<number>, selectedNodeInfos: TreeRowInfo[]) => {
+    documentManager.document.selectedItems.replace(selectedNodeInfos.map(info => this.getItem(info.node)))
   }
-  onSelectedKeysChange (selectedKeys: Set<number>, selectedNodeInfos: TreeRowInfo<Item>[]) {
-    documentManager.document.selectedItems.replace(selectedNodeInfos.map(info => info.item))
+  private onCollapsedChange = (info: TreeRowInfo, collapsed: boolean) => {
+    this.getItem(info.node).collapsed = collapsed
   }
-  onCollapsedChange (info: TreeRowInfo<Item>, collapsed: boolean) {
-    info.item.collapsed = collapsed
-  }
-  onContextMenu (info: TreeRowInfo<Item>|undefined, ev: React.MouseEvent<HTMLElement>) {
+  private onContextMenu = (info: TreeRowInfo|undefined, ev: React.MouseEvent<HTMLElement>) => {
     // TODO
   }
-  onMove (src: TreeRowInfo<Item>[], dest: TreeRowInfo<Item>, destIndex: number, destIndexAfter: number) {
-    const parent = dest.item
+  private onMove = (src: TreeRowInfo[], dest: TreeRowInfo, destIndex: number, destIndexAfter: number) => {
+    const parent = this.getItem(dest.node)
     if (!parent.canHaveChildren) {
       return
     }
     const beforeRef = parent.childAt(destIndex)
-    for (const {item} of src) {
+    for (const item of src.map(s => this.getItem(s.node))) {
       parent.insertBefore(item, beforeRef)
     }
     parent.document.versionControl.commit('Move Items')
   }
-  onCopy (src: TreeRowInfo<Item>[], dest: TreeRowInfo<Item>, destIndex: number) {
-    const parent = dest.item
+  private onCopy = (src: TreeRowInfo[], dest: TreeRowInfo, destIndex: number) => {
+    const parent = this.getItem(dest.node)
     if (!parent.canHaveChildren) {
       return
     }
     const beforeRef = parent.childAt(destIndex)
-    for (const {item} of src) {
+    for (const item of src.map(s => this.getItem(s.node))) {
       const cloned = cloneItems([item])[0]
       parent.insertBefore(cloned, beforeRef)
     }
     parent.document.versionControl.commit('Copy Items')
-  }
-}
-
-function peekAllItems (root: Item) {
-  for (const child of root.children) {
-    peekAllItems(child)
-  }
-}
-
-@observer
-export class ItemHierarchy extends React.Component<{}, {}> {
-  private delegate = new ItemTreeDelegate()
-
-  render () {
-    const ItemTreeView = TreeView as new () => TreeView<Item>
-    const document = documentManager.document
-    const selectedKeys = new Set([...document.selectedItems].map(item => item.id))
-
-    // FIXME: need to peek all items in ItemHierarchy#render to cause re-render
-    //        bacause TreeView in react-draggable-tree is not mobx observer.
-    peekAllItems(document.rootItem)
-
-    return <div className={styles.root}>
-      <ItemTreeView root={document.rootItem} selectedKeys={selectedKeys} rowHeight={28} delegate={this.delegate} />
-    </div>
   }
 }
