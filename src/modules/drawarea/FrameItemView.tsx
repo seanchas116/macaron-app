3b195261c5f8cae1e34ecf1fb9ddaada4f825243
Ryohei Ikegami <iofg2100@gmail.com>
2017-09-23 16:43:59 +0900

    Make movable

--- src/modules/drawarea/FrameItemView.tsx
+++ src/modules/drawarea/FrameItemView.tsx
@@ -3,15 +3,18 @@ import { observer } from 'mobx-react'
 
 import { FrameItem } from '../document'
 import { renderItem } from './renderItem'
+import { Movable } from './Movable'
 
 @observer
 export class FrameItemView extends React.Component<{item: FrameItem}, {}> {
   render () {
     const {item} = this.props
     const transform = `translate(${item.position.x}, ${item.position.y})`
-    return <g key={item.id} transform={transform}>
-      <rect x={0} y={0} width={item.size.x} height={item.size.y} fill={'transparent'} stroke={'none'} />
-      {[...item.children].reverse().map(renderItem)}
-    </g>
+    return <Movable item={item} key={item.id}>
+      <g transform={transform}>
+        <rect x={0} y={0} width={item.size.x} height={item.size.y} fill={'transparent'} stroke={'none'} />
+        {[...item.children].reverse().map(renderItem)}
+      </g>
+    </Movable>
   }
 }
