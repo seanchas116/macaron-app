79725316c8dec34c46f89b7450474febf74c0bb9
Ryohei Ikegami <iofg2100@gmail.com>
2017-09-23 16:42:01 +0900

    Render rect

--- src/modules/drawarea/FrameItemView.tsx
+++ src/modules/drawarea/FrameItemView.tsx
@@ -8,7 +8,9 @@ import { renderItem } from './renderItem'
 export class FrameItemView extends React.Component<{item: FrameItem}, {}> {
   render () {
     const {item} = this.props
-    return <g key={item.id}>
+    const transform = `translate(${item.position.x}, ${item.position.y})`
+    return <g key={item.id} transform={transform}>
+      <rect x={0} y={0} width={item.size.x} height={item.size.y} fill={'transparent'} stroke={'none'} />
       {[...item.children].reverse().map(renderItem)}
     </g>
   }
