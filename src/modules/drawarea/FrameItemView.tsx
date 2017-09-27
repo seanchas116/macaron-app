1a2adbf9d81270fd299a72921744be5f90aa25c5
Ryohei Ikegami <iofg2100@gmail.com>
2017-09-27 22:08:21 +0900

    Show border only if FrameItem is top level

--- src/modules/drawarea/FrameItemView.tsx
+++ src/modules/drawarea/FrameItemView.tsx
@@ -10,9 +10,18 @@ export class FrameItemView extends React.Component<{item: FrameItem}, {}> {
   render () {
     const {item} = this.props
     const transform = `translate(${item.position.x}, ${item.position.y})`
+    const isTopLevel = !!item.parent && !item.parent.parent
     return <Movable item={item} key={item.id}>
       <g transform={transform}>
-        <rect x={0} y={0} width={item.size.x} height={item.size.y} fill={'transparent'} stroke={'none'} />
+        <rect
+          x={0} y={0} width={item.size.x} height={item.size.y}
+          fill='transparent' stroke='transparent'
+        />
+        {isTopLevel &&
+          <rect
+            x={-0.5} y={-0.5} width={item.size.x + 0.5} height={item.size.y + 0.5}
+            fill='transparent' stroke='gray' strokeWidth={1}
+          />}
         {[...item.children].reverse().map(renderItem)}
       </g>
     </Movable>
