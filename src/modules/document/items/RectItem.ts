6ec08f6f60be56a1f5e3f0aba125b771be8903b4
Ryohei Ikegami <iofg2100@gmail.com>
2017-10-21 17:56:39 +0900

    Move radius to RectLikeItem

--- src/modules/document/items/RectItem.ts
+++ src/modules/document/items/RectItem.ts
@@ -1,16 +1,12 @@
-import { observable } from 'mobx'
-import { undoable } from './Item'
 import { RectLikeItem, RectLikeItemData } from './RectLikeItem'
 
 export interface RectItemData extends RectLikeItemData {
   type: 'rect'
-  radius: number
 }
 
 export
 class RectItem extends RectLikeItem {
   name = 'Rectangle'
-  @undoable @observable radius = 0
 
   loadData (data: RectItemData) {
     super.loadData(data)
--- src/modules/document/items/RectLikeItem.ts
+++ src/modules/document/items/RectLikeItem.ts
@@ -7,26 +7,31 @@ export interface RectLikeItemData extends ItemData {
   y: number
   width: number
   height: number
+  radius: number
 }
 
 export
 abstract class RectLikeItem extends Item {
   @undoable @observable position = new Vec2()
   @undoable @observable size = new Vec2()
+  @undoable @observable radius = 0
 
   loadData (data: RectLikeItemData) {
     super.loadData(data)
     this.position = new Vec2(data.x, data.y)
     this.size = new Vec2(data.width, data.height)
+    this.radius = data.radius
   }
 
   toData (): RectLikeItemData {
     const {x, y} = this.position
     const {width, height} = this.size
+    const {radius} = this
     return {
       ...super.toData(),
       x, y,
-      width, height
+      width, height,
+      radius
     }
   }
 }
